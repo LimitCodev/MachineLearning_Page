023703ba61fea26a2fb9af72a6983883bced1565
Alex Josue Ynocencio Callirgos <202017071+LimitCodev@users.noreply.github.com>
2026-04-20 20:59:30 -0500

    iluminar por medio de machine.js

--- machine.js
+++ machine.js
@@ -1,4 +1,8 @@
-function iniciarAnalisis(){
-  document.getElemntById("texto-estado").innerHTML="[SISTEMA ACTIVO] Escaneando algoritmos... Amenaza neutralizada.";
-  document.getElementById("texto-estado").style.color="#00ff00";
+function iluminar(idImagen) {
+    document.getElementById(idImagen).style.borderColor = "#ffffff";
+    document.getElementById(idImagen).style.boxShadow = "0px 0px 15px #ffcc00";
+}
+function apagar(idImagen) {
+    document.getElementById(idImagen).style.borderColor = "#ffcc00";
+    document.getElementById(idImagen).style.boxShadow = "none";
 }
