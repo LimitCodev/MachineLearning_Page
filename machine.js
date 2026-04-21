35aca9d5573e96735e31bb3ca16613653e6cc2e9
Alex Josue Ynocencio Callirgos <202017071+LimitCodev@users.noreply.github.com>
2026-04-20 19:40:54 -0500

    Inicio de archivo js

---
+++ machine.js
@@ -0,0 +1,4 @@
+function iniciarAnalisis(){
+  document.getElemntById("texto-estado").innerHTML="[SISTEMA ACTIVO] Escaneando algoritmos... Amenaza neutralizada.";
+  document.getElementById("texto-estado").style.color="#00ff00";
+}
